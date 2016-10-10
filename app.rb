001c00e9eee312af424411110989017bc4ac07cf
elizabethvenner <elizabethvenner@googlemail.com>
2016-10-10 15:14:48 +0100

    Add extra paths to app.rb and request shotgun

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
+require 'shotgun'
 
 get '/' do
-  "Hello!"
+  "Hello! Wazzzzup?"
 end
+
+get '/secret' do
+  "Dagmara and Elizabeth are going on a chocolate hunting quest."
+end
+
+get '/warning' do
+  "Sssh don't tell anyone!"
+  end
